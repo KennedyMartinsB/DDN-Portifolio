45fa708464db6f0be48fc2d58b28087cb215c5da
KennedyMartinsB <kenedy.martins185@gmail.com>
2026-03-13 01:35:35 -0300

    Feature: adição de imagens

--- ddn-portifolio/src/app/app.module.ts
+++ ddn-portifolio/src/app/app.module.ts
@@ -11,6 +11,7 @@ import { HomeComponent } from './pages/home/home.component';
 import { AboutComponent } from './pages/about/about.component';
 import { ContactComponent } from './pages/contact/contact.component';
 import { GalleryModalComponent } from './components/gallery-modal/gallery-modal.component';
+import { FooterComponent } from './components/footer/footer.component';
 // import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
@@ -22,7 +23,8 @@ import { GalleryModalComponent } from './components/gallery-modal/gallery-modal.
     HomeComponent,
     AboutComponent,
     ContactComponent,
-    GalleryModalComponent
+    GalleryModalComponent,
+    FooterComponent
 
   ],
   imports: [
